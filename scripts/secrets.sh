ca89daac6ee5c41a05e867507c98b3ebfccab60a
João Rocha <jmrs@dendroix.inescporto.pt>
2016-10-05 18:12:19 +0100

    separated secrets into another file

---
+++ scripts/secrets.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#dendro linux user -- who will own the installation folders and the dendro processes
+dendro_user_name='dendro'
+dendro_user_group='dendro'
+dendro_user_password='FIXME_____dendr0'
+
+#mysql
+mysql_username='root'
+mysql_root_password='FIXME_____r00t_p4ssw0rd'
+
+eudat_token="FIXME_____veryLongAndComplicatedString"
+
+#email account for messages and password recovery messaging
+emailing_account_gmail_user="FIXME_____gmail_user_to_send_emails"
+emailing_account_gmail_password="FIXME_____password_for_gmail_user_to_send_emails"
+
+#gmaps API key
+gmaps_api_key="FIXME_____VERY_LONG_AND_COMPLICATED_STRING"
+
+#virtuoso
+
+#admin user
+virtuoso_dba_user="dba"
+virtuoso_dba_password="FIXME_____dba"
+
+#virtuoso linux user (owner of the virtuoso installation and process)
+virtuoso_user='virtuoso'
+virtuoso_group='virtuoso'
+virtuoso_user_password='FIXME_____virtu0s0'
+
+#mongodb
+mongodb_dba_user="root"
+mongodb_dba_password="r00t"
