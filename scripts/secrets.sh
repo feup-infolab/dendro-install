08d17fe24d4ddd5462704920656f78d1fe1fca24
João Rocha da Silva <joaorosilva@gmail.com>
2016-10-06 22:54:07 +0100

    Update secrets.sh

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
 #dendro linux user -- who will own the installation folders and the dendro processes
-dendro_user_name='FIXME_____dendro'
-dendro_user_group='FIXME_____dendro'
-dendro_user_password='FIXME_____dendr0'
+dendro_user_name='dendro'
+dendro_user_group='dendro'
+dendro_user_password='dendr0'
 
 #mysql
-mysql_username='FIXME_____root'
-mysql_root_password='FIXME_____r00t_p4ssw0rd'
+mysql_username='root'
+mysql_root_password='r00t_p4ssw0rd'
 
-eudat_token="FIXME_____veryLongAndComplicatedString"
+eudat_token="veryLongAndComplicatedString"
 
 #email account for messages and password recovery messaging
-emailing_account_gmail_user="FIXME_____gmail_user_to_send_emails"
-emailing_account_gmail_password="FIXME_____password_for_gmail_user_to_send_emails"
+emailing_account_gmail_user="gmail_user_to_send_emails"
+emailing_account_gmail_password="password_for_gmail_user_to_send_emails"
 
 #gmaps API key
-gmaps_api_key="FIXME_____VERY_LONG_AND_COMPLICATED_STRING"
+gmaps_api_key="VERY_LONG_AND_COMPLICATED_STRING"
 
 #virtuoso
 
 #admin user
-virtuoso_dba_user="FIXME_____dba"
-virtuoso_dba_password="FIXME_____dba"
+virtuoso_dba_user="dba"
+virtuoso_dba_password="dba"
 
 #virtuoso linux user (owner of the virtuoso installation and process)
 virtuoso_user='virtuoso'
 virtuoso_group='virtuoso'
-virtuoso_user_password='FIXME_____virtu0s0'
+virtuoso_user_password='virtu0s0'
 
 #mongodb
-mongodb_dba_user="FIXME_____root"
-mongodb_dba_password="FIXME_____r00t"
+mongodb_dba_user="root"
+mongodb_dba_password="r00t"
 
 #svn users and passwords
-svn_user="FIXME_____user_for_dendro_svn_repo"
-svn_user_password="FIXME_____password_for_dendro_svn_repo"
+svn_user="user_for_dendro_svn_repo"
+svn_user_password="password_for_dendro_svn_repo"
