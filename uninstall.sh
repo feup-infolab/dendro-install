870db0582252bce4330fc7ba1b6e0740a1776c6e
João Rocha da Silva <joaorosilva@gmail.com>
2016-12-13 17:02:27 +0000

    Fixing uninstall
    Fixing uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 source ./scripts/constants.sh
 
-
-
 machine_exists()
 {
   vbox_name=$3
@@ -20,11 +18,10 @@ machine_exists()
     elif [[ "$OSTYPE" == "freebsd"* ]] || [[ "$OSTYPE" == "darwin"* ]]
     then
       found_vbox_id=$(echo $fetched_vbox_line | grep -o "{.*}" | grep -o "[^{}]*[^{}]" )
-      echo $found_box_id
     fi
 
     if [ "$found_vbox_id" != "" ]; then
-      info "vbox_exists : $vbox_exists. fetched_vbox_id: $found_vbox_id"
+      #info "vbox_exists : $vbox_exists. fetched_vbox_id: $found_vbox_id"
   		eval "$2=$found_vbox_id"
   	fi
 
@@ -43,13 +40,21 @@ then
   info "Virtualbox ${active_deployment_setting} was found and has ID: $vbox_id"
 
   vagrant destroy -f ${active_deployment_setting} || warning "Unable to destroy VM ${active_deployment_setting}"
-  VBoxManage controlvm $vbox_id poweroff || warning "Unable to power off VM ${active_deployment_setting}. Does it exist?"
-  VBoxManage unregistervm $vbox_id -delete || warning "Unable to delete VM ${active_deployment_setting}."
-
-  rm -rf .\.vagrant
-
   #clean list of VMs
   vagrant global-status --prune || true
+
+  #if failed, try to destroy via VirtualBox
+  vbox_exists="false"
+  vbox_id=""
+  machine_exists vbox_exists vbox_id ${active_deployment_setting}
+
+  if [ "$vbox_exists" == "true" ] && [ $vbox_id != "" ]
+  then
+    VBoxManage controlvm $vbox_id poweroff || warning "Unable to power off VM ${active_deployment_setting}. Does it exist?"
+    VBoxManage unregistervm $vbox_id -delete || warning "Unable to delete VM ${active_deployment_setting}."
+    rm -rf "$HOME/Virtualbox\ VMs\\${active_deployment_setting}"
+    vagrant global-status --prune || true
+  fi
 
   vbox_exists="false"
   vbox_id=""
@@ -59,6 +64,8 @@ then
   then
     die "Virtualbox still exists, unable to delete"
   fi
+
+  rm -rf .\.vagrant
 else
   warning "Virtualbox does not exist, there is nothing to delete."
 fi
