1628a514cb4e9e7889a114764573957232944205
silvae86 <joaorosilva@gmail.com>
2017-01-01 17:45:31 +0000

    Added message to wait while installing JDK
    Added message to wait while installing JDK

--- scripts/Dependencies/oracle_jdk8.sh
+++ scripts/Dependencies/oracle_jdk8.sh
@@ -13,12 +13,13 @@ fi
 #save current dir
 setup_dir=$(pwd) &&
 
-info "Installing Java 8 JDK"
+info "Installing Java 8 JDK. This will take a few minutes."
 sudo apt-get install --yes python-software-properties
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update -qq
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo /usr/bin/debconf-set-selections
+info "This will take a few minutes and no messages will be shown. PLEASE WAIT!"
 sudo apt-get install -qq --yes oracle-java8-installer > /dev/null
 yes "" | sudo apt-get -f install > /dev/null || die "There was an error installing Oracle JDK 8."
 
