9b98ee5b2234e3ca4e7306305a800a759e74528e
João Rocha da Silva <joaorosilva@gmail.com>
2017-01-14 21:51:51 +0000

    Adding -s flag for restoring last snapshot
    Adding -s flag for restoring last snapshot

--- install.sh
+++ install.sh
@@ -11,37 +11,50 @@ chmod -R 0777 ~/.vagrant.d
 
 SHELL_ARGS=''
 
-while getopts 'atcjudrb:' flag; do
+while getopts 'atcjudrbs:' flag; do
   case $flag in
+    s)
+      #revert to last snapshot
+      snapshot_id=VBoxManage snapshot $active_deployment_setting list | tail -n 1 | grep -o "UUID.*" | cut -c 7-42
+      VBoxManage snapshot $active_deployment_setting restore $snapshot_id
+      ;;
     a)
+      #install TeamCity
       take_vm_snapshot $active_deployment_setting "install_teamcity_agent"
     	VAGRANT_SHELL_ARGS=$VAGRANT_SHELL_ARGS'-a '
     	;;
     c)
+      #install TeamCity Agent
       take_vm_snapshot $active_deployment_setting "install_teamcity"
     	VAGRANT_SHELL_ARGS=$VAGRANT_SHELL_ARGS'-c '
     	;;
     t)
+      #Run Tests on Dendro after checkout
       take_vm_snapshot $active_deployment_setting "run_tests"
     	VAGRANT_SHELL_ARGS=$VAGRANT_SHELL_ARGS'-t '
     	;;
     r)
+      #refresh only the code of Dendro without installing dependencies
       take_vm_snapshot $active_deployment_setting "refresh_code_only"
 		  VAGRANT_SHELL_ARGS=$VAGRANT_SHELL_ARGS'-r '
 		  ;;
     d)
+      #enable development mode
       take_vm_snapshot $active_deployment_setting "set_dev_mode"
 		  VAGRANT_SHELL_ARGS=$VAGRANT_SHELL_ARGS'-d '
 		  ;;
     u)
+      #disable development mode
       take_vm_snapshot $active_deployment_setting "unset_dev_mode"
 		  VAGRANT_SHELL_ARGS=$VAGRANT_SHELL_ARGS'-u '
 	    ;;
     j)
+      #Install Jenkins
       take_vm_snapshot $active_deployment_setting "install_jenkins"
   		VAGRANT_SHELL_ARGS=$VAGRANT_SHELL_ARGS'-j '
   		;;
 	  b)
+      #Checkout Dendro branch specified as argument
       take_vm_snapshot $active_deployment_setting "install_with_branch_$OPTARG"
 		  VAGRANT_SHELL_ARGS="$VAGRANT_SHELL_ARGS-b $OPTARG "
 		  ;;
