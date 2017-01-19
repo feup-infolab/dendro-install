dacef5e085f1584834cafea7ffd626d93d31e4c0
silvae86 <joaorosilva@gmail.com>
2017-01-19 00:01:24 +0000

    Added installation via .deb package
    Added installation via .deb package

--- scripts/Dependencies/virtuoso_from_deb.sh
+++ scripts/Dependencies/virtuoso_from_deb.sh
@@ -8,18 +8,51 @@ else
 	source ./constants.sh
 fi
 
-info "Installing Virtuoso 7.1.1 From PPA......"
+info "Installing Virtuoso 7.2.4 from PPA @feup-infolab/virtuoso7-debs."
 
 #save current dir
 setup_dir=$(pwd)
 
-#install Virtuoso 7.1.1 from PPA
+#install Virtuoso 7.2.4 from PPA
 
 git clone https://github.com/feup-infolab/virtuoso7-debs.git virtuoso7
 cd virtuoso7/debs-ubuntu-16-04
-sudo dpkg -i virtuoso-opensource7_7.2.4-1_amd64.deb
+sudo dpkg -i virtuoso-opensource*.deb
+
+#setup default configuration .ini file
+sudo cp /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso.ini.sample /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso.ini
+
+#create virtuoso user and give ownership
+sudo useradd $virtuoso_user
+sudo addgroup $virtuoso_group
+sudo usermod $virtuoso_user -g $virtuoso_group
+echo "${virtuoso_user}:${virtuoso_user_password}" | sudo chpasswd
+sudo chown -R $virtuoso_user:$virtuoso_group /usr/local/virtuoso-opensource
+
+# Setting crontab to reboot virtuoso every day at 4am (likely has memory leaks...)
+
+	#from http://stackoverflow.com/questions/23309120/setting-cron-task-every-day-at-2am-makes-it-run-every-minute
+	# This runs the script every minute of 2am (02:00, 02:01, 02:02 and so on):
+	#
+	#  * 2 * * *
+	#  While This runs the script at 02:13am (of each day of each month)
+	#
+	#  13 2 * * *
+	#
+	#  * * * * *  command to execute
+	#  ┬ ┬ ┬ ┬ ┬
+	#  │ │ │ │ │
+	#  │ │ │ │ │
+	#  │ │ │ │ └───── day of week (0 - 7) (0 to 6 are Sunday to Saturday, 7 is 	Sunday again)
+	#  │ │ │ └────────── month (1 - 12)
+	#  │ │ └─────────────── day of month (1 - 31)
+	#  │ └──────────────────── hour (0 - 23)
+	#  └───────────────────────── min (0 - 59)
+
+add_line_to_file_if_not_present "00 4 * * *   root    test -x /usr/sbin/anacron || ( systemctl restart virtuoso  )" "/etc/crontab" ||
+die "Failed to set Virtuoso OpenSource crontab"
 
 #go back to initial dir
 cd $setup_dir
 
-success "Installed Subversion 1.8."
+success "Installed Virtuoso 7.2.4 from PPA @feup-infolab/virtuoso7-debs."
