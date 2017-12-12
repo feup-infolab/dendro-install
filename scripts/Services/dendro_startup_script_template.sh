f59b44e0ecd80dcce5b3556c62382d6773252d0a
silvae86 <joaorosilva@gmail.com>
2017-12-12 23:39:52 +0000

    Refactored dendro startup template file

--- scripts/Services/dendro_startup_script_template.sh
+++ scripts/Services/dendro_startup_script_template.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
-echo "[[Dendro starting...]]"
+echo "[[ Dendro starting...]]" 
+
 export NVM_DIR="$HOME/.nvm" &&
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-echo "nvm is at: $NVM_DIR"
+
+echo "[[ nvm location: $NVM_DIR ]]"
+echo "[[ node location: $(which node) ]]"
+echo "[[ node version: $(node -v) ]]"
+echo "[[ user running the script: $(whoami) ]]"
+echo "[[ dendro installation path: %DENDRO_INSTALLATION_PATH% ]]"
+echo "[[ dendro log location: %DENDRO_LOG_FILE% ]]"
 
 nvm use %NODE_VERSION% --delete-prefix &&
-
-echo "node is at: $(which node)"
-echo "node version: $(node -v)"
-
-pm2 list
-node src/app.js | tee --append %DENDRO_LOG_FILE%
+pm2 list &&
+node %DENDRO_INSTALLATION_PATH%/src/app.js | tee --append %DENDRO_LOG_FILE%
 	
 if [[ "$?" != "0" ]]
 then
+	echo "There was an error starting Dendro Service!!!"
 	exit 1
 fi
 
-#node %DENDRO_INSTALLATION_PATH%/src/app.js 
+
