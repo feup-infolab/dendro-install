b4c4a84a4091b6cd4f5dd6c41a011e13b596f9eb
João Rocha da Silva <joaorosilva@gmail.com>
2018-03-21 16:53:29 +0000

    Added function to wait for virtuoso to boot before attempting to load base ontologies.

--- scripts/SQLCommands/grant_commands.sh
+++ scripts/SQLCommands/grant_commands.sh
@@ -15,6 +15,20 @@ else
 	running_folder=$script_dir/SQLCommands
 fi
 
+function wait_for_virtuoso_to_boot()
+{
+    echo "Waiting for virtuoso to boot up..."
+    attempts=0
+    max_attempts=30
+    while ( nc 127.0.0.1 8890 < /dev/null || nc 127.0.0.1 1111 < /dev/null )  && [[ $attempts < $max_attempts ]] ; do
+        attempts=$((attempts+1))
+        sleep 1;
+        echo "waiting... (${attempts}/${max_attempts})"
+    done
+}
+
+wait_for_virtuoso_to_boot
+
 # change the default password if it is set as default and the password is different
 if [[ "${virtuoso_dba_password}" != "dba"]]; then
 	echo "set password dba ${virtuoso_dba_password};" | /usr/local/virtuoso-opensource/bin/isql 127.0.0.1 "dba" "dba"
