b5f5e9767769e24c225aaf3e5e1a330be19c969f
João Rocha da Silva <joaorosilva@gmail.com>
2018-07-11 02:31:34 +0100

    More fixes...

--- reinstall.sh
+++ reinstall.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#save current directory
+starting_dir=$(pwd)
+
 source ./scripts/constants.sh
 
 read -p "Are you sure that you want to delete the VM named '${active_deployment_setting}' and reinstall it? [y/n]" -n 1 -r
@@ -9,11 +12,17 @@ then
 	#echo "Please enter your administrator password:"
 	#destroy dendro vm
 
+	info "Deleting all *.vmdk in $starting_dir"
+	rm $starting_dir/*.vmdk
+
+	info "Deleting all *.vdi in $starting_dir"
+	rm $starting_dir/*.vdi
+
 	[[ "$active_deployment_setting" -ne "" ]]
-	echo "Active dep setting not empty? $?"
+	info "Active dep setting not empty? $?"
 
 	[[ -d "$HOME/VirtualBox VMs/$active_deployment_setting" ]]
-	echo "Folder exists? $?"
+	info "Folder exists? $?"
 
 	if [ "$active_deployment_setting" != "" ] && [ -d "$HOME/VirtualBox VMs/$active_deployment_setting" ];
 	then
